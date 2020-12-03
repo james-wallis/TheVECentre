fdef5d8aab8491d32fb2bafcf668e47cbc0fac11
James Wallis <j@wallis.dev>
2020-12-03 20:28:54 +0000

    Use Sharp for image resize
    Signed-off-by: James Wallis <j@wallis.dev>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
   /* config for next-optimized-images */
-
+  loader: 'responsive-loader',
+  responsive: {
+    adapter: require('responsive-loader/sharp'),
+  },
   // your config for other plugins or the general next.js here...
 });
