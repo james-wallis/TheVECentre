54e1b7ec752a385f670a9ac0c595a5a4a16af36e
James Wallis <j@wallis.dev>
2020-12-17 11:33:04 +0000

    christmas tour redirect
    Signed-off-by: James Wallis <j@wallis.dev>

--- pages/api/tours/[tour].tsx
+++ pages/api/tours/[tour].tsx
@@ -8,6 +8,9 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
         case 'main-hall':
             redirectURL = 'https://thevecentre.b-cdn.net/TheVECentreXmasTour/index.htm';
             break;
+        case 'christmas':
+            redirectURL = 'https://thevecentre.s3.eu-west-2.amazonaws.com/christmas/index.htm';
+            break;
     }
 
     if (!redirectURL) {
