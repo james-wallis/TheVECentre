2df9ccfb24d4cabd9c44e9077985476c637d3156
James Wallis <j@wallis.dev>
2021-01-11 21:24:49 +0000

    Remove mobile redirect
    Signed-off-by: James Wallis <j@wallis.dev>

--- pages/tours/[...tour].tsx
+++ pages/tours/[...tour].tsx
@@ -60,23 +60,23 @@ function getTourURLs(tour: string[]): string[] {
 }
 
 async function shouldRedirect(): Promise<boolean> {
-    const ua = (function() { return window.navigator.userAgent.toLowerCase(); })();
-    const inUA = function(value: string) { return (ua.search(value.toLowerCase()) >= 0); };
-    const mobile: boolean = inUA('android') || inUA('ios') || inUA('iphone') || inUA('ipod') || inUA('ipad');
+    // const ua = (function() { return window.navigator.userAgent.toLowerCase(); })();
+    // const inUA = function(value: string) { return (ua.search(value.toLowerCase()) >= 0); };
+    // const mobile: boolean = inUA('android') || inUA('ios') || inUA('iphone') || inUA('ipod') || inUA('ipad');
 
-    if (mobile) {
-        return true;
-    } else if (navigator.getVRDisplays) {
-        try {
-            const displays: any = await navigator.getVRDisplays();
-            if (displays.length > 0)
-                return true;
-            else
-                return false;
-        } catch(err) {
-            return false;
-        }
-    }
+    // if (mobile) {
+    //     return true;
+    // } else if (navigator.getVRDisplays) {
+    //     try {
+    //         const displays: any = await navigator.getVRDisplays();
+    //         if (displays.length > 0)
+    //             return true;
+    //         else
+    //             return false;
+    //     } catch(err) {
+    //         return false;
+    //     }
+    // }
     return false;
 }
 
