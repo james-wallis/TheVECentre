958c277875314afb9cafc588d8daab86138f6921
James Wallis <j@wallis.dev>
2021-03-05 14:53:34 +0000

    add gallery jewellery link
    Signed-off-by: James Wallis <j@wallis.dev>

--- tours.ts
+++ tours.ts
@@ -2,6 +2,17 @@ import { ITour } from './interfaces';
 
 const tours: ITour[] = [
     {
+        title: 'ArtistJodi\'s gallery',
+        description: 'See all of ArtistJodi\'s work in her bespoke 3D gallery.',
+        path: 'artistjodi',
+        subpaths: {
+            gallery: 1,
+            studio: 49,
+            courtyard: 4,
+            jewellery: 13,
+        }
+    },
+    {
         title: 'The VECentre tour',
         description: 'The immersive 3D tour brought to you by TheVECentre.',
         path: 'main',
@@ -23,16 +34,6 @@ const tours: ITour[] = [
             craft: 18,
             santa: 22,
             market: 7
-        }
-    },
-    {
-        title: 'ArtistJodi\'s gallery',
-        description: 'See all of ArtistJodi\'s work in her bespoke 3D gallery.',
-        path: 'artistjodi',
-        subpaths: {
-            gallery: 1,
-            studio: 49,
-            courtyard: 4,
         }
     }
 ];
