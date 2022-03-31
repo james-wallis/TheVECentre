eb6177ba6ae64e612449e895c490484b3a0bd493
James Wallis <j@wallis.dev>
2022-03-31 14:20:38 +0100

    chore: pr comments

--- components/HeroImage.tsx
+++ components/HeroImage.tsx
@@ -1,6 +1,4 @@
 import React, { ReactNode } from "react"
-import { ResponsiveImage } from './Image'
-import { EnterTourIcon } from "./Icons"
 
 interface IProps {
     src: string
@@ -18,14 +16,12 @@ const getIconPlacement = (placement: string | undefined) => {
     return `header-${placement}`
 }
 
-const HeroImage = ({ src, icon, placement, dark, href }: IProps) => (
+const HeroImage = ({ icon, placement }: IProps) => (
     <div className="mb-4 relative h-hero-mobile md:h-auto h-hero-img">
         {icon && (
             <div className={`absolute h-full w-full flex justify-center items-center lg:pt-8 ${getIconPlacement(placement)}`}>
-                <EnterTourIcon dark={dark} href={href || ''} />
             </div>
         )}
-        <ResponsiveImage className="w-screen h-full md:h-hero-img object-cover" src={require(`../images/heros/${src}?sizes[]=250,sizes[]=500,sizes[]=750,sizes[]=1000,sizes[]=1250,sizes[]=1500,sizes[]=1750,sizes[]=2000`)} alt='hero image' />
     </div>
 )
 
