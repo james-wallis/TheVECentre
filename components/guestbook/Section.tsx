00589c08bf91da2664948edb5c46a4de4d0272c5
James Wallis <j@wallis.dev>
2022-04-17 22:21:30 +0100

    refactor: Guestbook styling tweaks

--- components/guestbook/Section.tsx
+++ components/guestbook/Section.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 const Section = ({ children, variant, rightSide = false }: IProps) => (
     <div className={`
-        h-full w-3/4 md:w-1/2 py-20
+        h-full w-3/4 md:w-1/2 py-10 md:py-20
         ${rightSide
             ? `hidden md:block md:pr-8 lg:pr-24 xl:pr-28 md:pl-20 lg:pl-32`
             : `md:pl-8 lg:pl-24 xl:pl-28 md:pr-20 lg:pr-32 ${variant === "thevecentre" ? "md:border-r border-gray-700" : ""}`
--- components/guestbook/TheVECentreTitle.tsx
+++ components/guestbook/TheVECentreTitle.tsx
@@ -13,10 +13,10 @@ const TheVECentreTitle = ({ large = false }: IProps) => (
             width="140"
             height="140"
         />
-        <h1 className={`font-light ${large ? 'text-5xl mt-8' : 'text-4xl'}`}>
+        <h1 className={`font-light ${large ? 'text-4xl md:text-5xl mt-6 md:mt-8' : 'text-3xl md:text-4xl'}`}>
             <span className="block">The VECentre</span>
         </h1>
-        <p className={`${large ? 'text-3xl' : 'text-2xl'} font-extralight text-gray-400 mt-2`}>Guestbook</p>
+        <p className={`${large ? 'text-2xl md:text-3xl' : 'text-xl md:text-2xl'} font-extralight text-gray-400 mt-2`}>Guestbook</p>
     </div>
 )
 
